842a1054af73cfbd61bdb30af3abd074176aadb3
cecmunozdom-del <cec.munozdom@gmail.com>
2026-09-19 15:14:56 +0200

    avance js y casillero

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Uso de IA:
+-Uso de IA: 
+Inicialmente utilicé IA para que me resolviera dudas acerca de HTML, CSS y JS, ya que al principio había cosas de las que no me acordaba bien, o no entendía del todo. También me fue útil a la hora de formar el tablero dinámico con los inputs en cada celda. Busqué varios tutoriales y páginas web acerca del tema, pero siempre usaban métodos que no me convencían del todo, así que le pedí a Gemini que me diera un ejemplo de cómo hacerlo, y gracias a esto pude adaptarlo a mi manera en mi código.
 
 
-Autopsia:
+Algunos ejemplos de prompts que he hecho:
+
+
+
+
+-Autopsia: 
+Decisión difícil 1: inputs en el tablero. No estaba segura sobre la forma de introducir 'o' y 's' en el tablero, porque la idea inicial era simplemente poner click derecho 'o' y click izquierdo 's', o viceversa, pero finalmente decidí hacer que todas las casillas del tablero fueran inputs, de manera que se puede escribir el texto directamente. El click derecho lo dejé para marcar casillas vacías en las que escribir, y el click izquierdo para marcar las casillas ya seleccionadas a la hora de formar 'oso'.
--- oso.js
+++ oso.js
@@ -1,1 +1,49 @@
-//var side = window.prompt("Hola soy homelo chino");
+const tam = 10; //variable del tamaño del lado del tablero, para poder cambiarlo en cualquier momento
+
+function formarCasillero(lado){
+    const casillero = document.getElementById('casillero');
+
+    //innerHTML modifica el contenido dentro de un elemento del DOM
+    //en este caso nos sirve para "vaciar" todo el casillero
+    casillero.innerHTML = "";
+
+    //divisón en filas y columnas
+    casillero.style.gridTemplateColumns = `repeat(${lado}, 1fr)`;
+    casillero.style.gridTemplateRows = `repeat(${lado}, 1fr)`;
+
+    let casillas = lado * lado;
+    for(let i = 0; i < casillas; i++){
+        let casilla = document.createElement('div');
+        casilla.classList.add('casilla'); //!! esto le añade al class="casilla" de html esta clase casilla concreta
+                                         //así desde css va a ser más fácil decorarlas
+        casilla.innerHTML = `<input type='text' class='input_casilla' id=input_casilla_num${i}>`; //una clase para todas las casillas, un id para cada una por si acaso
+        // TODO: borrar eta vaina -> casilla.addEventListener('click', () => {console.log(`Has pulsado la casilla número ${i}`);}); LAS CASILLAS VAN NUMERADAS ESO ES GOOOOD
+
+        casillero.appendChild(casilla);
+    }
+
+}
+
+formarCasillero(tam); 
+
+
+//TODO
+function flujoPartida(){
+
+
+
+
+
+
+}
+
+
+
+//lo separo en 2 porque queremos distintos colores en el hover y tal pero igual acaba no haciendo falta
+function turnoJugador1(){
+
+}
+
+function turnoJugador2(){
+
+}
