c790cba2fc70b6bffcdaefff726ada0a730bedf9
cecmunozdom-del <cec.munozdom@gmail.com>
2026-09-19 23:43:28 +0200

    avances de la tarde

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 -Uso de IA: 
 Inicialmente utilicé IA para que me resolviera dudas acerca de HTML, CSS y JS, ya que al principio había cosas de las que no me acordaba bien, o no entendía del todo. También me fue útil a la hora de formar el tablero dinámico con los inputs en cada celda. Busqué varios tutoriales y páginas web acerca del tema, pero siempre usaban métodos que no me convencían del todo, así que le pedí a Gemini que me diera un ejemplo de cómo hacerlo, y gracias a esto pude adaptarlo a mi manera en mi código.
 
+También me dio la idea de que para cambiar el color de las casillas con hover según el turno del jugador, en lugar de hacer varios bucles en js, podía simplemente usar combinaciones de selectores en css, así que investigué eso por mi cuenta y es lo que acabé haciendo, ya que quedaba más limpio y ordenado, y era menos código.
+
 
 Algunos ejemplos de prompts que he hecho:
 
--- oso.js
+++ oso.js
@@ -1,5 +1,9 @@
 const tam = 10; //variable del tamaño del lado del tablero, para poder cambiarlo en cualquier momento
+const casillero = document.getElementById('casillero');
+const casillasLibres = [];
+const casillasOcupadas = [];
 
+//FORMACIÓN DEL TABLERO
 function formarCasillero(lado){
     const casillero = document.getElementById('casillero');
 
@@ -14,12 +18,13 @@ function formarCasillero(lado){
     let casillas = lado * lado;
     for(let i = 0; i < casillas; i++){
         let casilla = document.createElement('div');
-        casilla.classList.add('casilla'); //!! esto le añade al class="casilla" de html esta clase casilla concreta
-                                         //así desde css va a ser más fácil decorarlas
+        casilla.classList.add('casilla'); //!! esto le añade al class="casilla" de html esta clase casilla concreta, así desde css va a ser más fácil decorarlas
         casilla.innerHTML = `<input type='text' class='input_casilla' id=input_casilla_num${i}>`; //una clase para todas las casillas, un id para cada una por si acaso
-        // TODO: borrar eta vaina -> casilla.addEventListener('click', () => {console.log(`Has pulsado la casilla número ${i}`);}); LAS CASILLAS VAN NUMERADAS ESO ES GOOOOD
 
-        casillero.appendChild(casilla);
+        casilla.addEventListener('click', casillaPulsada(casilla));
+
+        casillasLibres.add(casilla); //añadimos la casilla al array de casillas vacías
+        casillero.appendChild(casilla); //añadimos la casilla al casillero
     }
 
 }
@@ -27,23 +32,97 @@ function formarCasillero(lado){
 formarCasillero(tam); 
 
 
-//TODO
+//TODO: FLUJO DE LA PARTIDA
 function flujoPartida(){
 
+    let turno = 1;
+    
+
+    while(comprobarFinPartida){
+
+        //TURNO J1
+        if(turno%2 != 0){
+            
+        //comprobar sobre la casilla a la que le ha hecho click:
+        casilla = document.getElementById('casilla1'); //TODO: esto tengo que ver cómo lo hago
+
+        //va a poner letra
+        if(detectorClick(casilla) === 1){
+
+
+        //va a marcar OSO
+        }else{
+            comprobarOSO();
+        }
+
+
+
+        //TURNO J2
+        }else{
+            
 
 
 
 
+
+        }
+
+        turno++;
+    }
+
+    
 
 }
 
+function detectorClick(casilla){
+    casilla.addEventListener('mousedown', function(event){
+        if(casilla.button === 0){
+            return 1;
+        }else if(casilla.button === 1){
+            return 2;
+        }else{
+            return 0;
+        }
+    });
+}
+
+function casillaPulsada(casilla){
+    casillasOcupadas.add(casilla);
+}
+
+function turnoJ1(){
+    casillero.classList.add('turnoJ1'); //añade una clase y quita otra
+    casillero.classList.remove('turnoJ2');
+}
+
+function turnoJ2(){
+    casillero.classList.add('turnoJ2'); //añade una clase y quita otra
+    casillero.classList.remove('turnoJ1');
+}
+
+function comprobarOSO(){
+
+    //CONDICIONES OSO:
+    /*
+    1. la primera O es una casilla ocupada
+    2. la S es una de las casillas de alrededor y está ocupada
+    3. la segunda O es la que continúa a la S y también está ocupada
 
 
-//lo separo en 2 porque queremos distintos colores en el hover y tal pero igual acaba no haciendo falta
-function turnoJugador1(){
+
+
+
+
+
+    */
 
 }
 
-function turnoJugador2(){
+function comprobarFinPartida(casillasOcupadas){
+    if(casillasOcupadas < (tam*tam)){
+        return true;
+    }else{
+        false;
+    }
 
 }
